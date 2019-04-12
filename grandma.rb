8a2a50e13bf39fb21eaf74ebe6fb6fc49879fd87
James Declerus <james.declerus@esih.edu>
2019-04-12 13:24:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@ def speak_to_grandma(phrase = " ")
       return "HUH?! SPEAK UP, SONNY!"
 
     elsif phrase == phrase.upcase
-      puts "NO, NOT SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
     # NO, NOT SINCE 1938!
     else phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
