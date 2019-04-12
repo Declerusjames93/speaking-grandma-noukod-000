a19370105778003e67c1602e215d1e9219d0af67
James Declerus <james.declerus@esih.edu>
2019-04-12 12:40:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,7 +9,7 @@ def speak_to_grandma(phrase = " ")
 
     # NO, NOT SINCE 1938!
     else phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
+      puts "I LOVE YOU TOO PUMPKIN!"
 
     end
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
